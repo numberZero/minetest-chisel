fffe63079e40c7ca695ef1a2e3311412b838e589
numzero <numzer0@yandex.ru>
2020-12-02 00:07:18 +0300

    Checkpoint

--- chisel.py
+++ chisel.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import sys, os, traceback, time
+import sys, os, os.path, traceback, time, types
 if 'WAYLAND_DISPLAY' in os.environ:
 	os.environ['PYOPENGL_PLATFORM'] = 'egl' # PyOpenGL такого не умеет, WTF?
 
@@ -37,39 +37,86 @@ class Part:
 	indices = np.indices((16, 16, 16), dtype='int32').transpose().reshape((-1, 3))
 
 	def __init__(self):
-		#self.data = np.full((16, 16, 16), 1)
-		self.data = np.fromiter(rand(), 'int32', 16*16*16).reshape(16, 16, 16)
+		self.data = np.full((16, 16, 16), 1, 'int32')
+		#self.data = np.fromiter(rand(), 'int32', 16*16*16).reshape(16, 16, 16)
 
-part = Part()
+class GLPart(Part):
+	index_buf = 0
+
+	def __init__(self):
+		super(GLPart, self).__init__()
+		self.tex = 0
+
+	@classmethod
+	def _initGL_shared(cls):
+		if cls.index_buf:
+			return
+
+	def initGL(self):
+		self._initGL_shared()
+		if self.tex:
+			return
+		self.tex = int(glGenTextures(1))
+		glBindTexture(GL_TEXTURE_3D, self.tex) # создание 3D-текстуры
+		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+		glTexStorage3D(GL_TEXTURE_3D, 1, GL_R8I, 16, 16, 16)
+		self.updateGL()
+
+	def updateGL(self):
+		glTextureSubImage3D(self.tex, 0, 0, 0, 0, 16, 16, 16, GL_RED_INTEGER, GL_INT, self.data)
+		glTextureParameteriv(self.tex, GL_TEXTURE_BORDER_COLOR, (1, 2, 3, 4))
+
+part = GLPart()
 sel = None
+
+class Face:
+	def __init__(self, u, v, w, name):
+		self.u = u
+		self.v = v
+		self.w = w
+		self.name = name
+
+faces = [
+	Face((1, 0, 0), (0, 1, 0), (0, 0, 1), "back"),
+	Face((0, 0, 1), (1, 0, 0), (0, 1, 0), "top"),
+	Face((0, 1, 0), (0, 0, 1), (1, 0, 0), "right"),
+	Face((0, 1, 0), (1, 0, 0), (0, 0, -1), "front"),
+	Face((1, 0, 0), (0, 0, 1), (0, -1, 0), "bottom"),
+	Face((0, 0, 1), (0, 1, 0), (-1, 0, 0), "left"),
+];
 
 class ChiselView(QtWidgets.QOpenGLWidget):
 	def __init__(self, *args, **kwargs):
 		super(ChiselView, self).__init__(*args, **kwargs)
 		self.rotate = np.identity(4)
 		self.mouse = None
+		self.scale = 1
 		self.setMouseTracking(True)
 
 	def updateMouse(self):
 		global sel
 		if not self.mouse:
 			return
-		x, y, z, p = self.img[self.img.shape[0] - self.mouse[1] - 1, self.mouse[0]]
+		try:
+			x, y, z, p = self.img[self.img.shape[0] - self.mouse[1] - 1, self.mouse[0]]
+		except IndexError:
+			p = 255
 		if p == 255:
-			nsel = None
+			sel = None
 			mainWindow.hover.setText('none')
-		else:
-			nsel = (p, x, y, z)
-			mainWindow.hover.setText(f'{p}: ({x}, {y}, {z})')
+			return
+		p, f = p & 0x1F, p >> 5
+		nsel = (p, x, y, z, f)
 		if nsel != sel:
 			sel = nsel
-			mainWindow.viewTop.update()
-			mainWindow.viewRight.update()
-			mainWindow.viewFront.update()
-			mainWindow.viewUser.update()
+			facename = faces[f].name
+			mainWindow.hover.setText(f'{p}: ({x}, {y}, {z}) {facename}')
+			updateViews()
 
 	def updateFramebuffer(self):
-		w, h = self.pw, self.ph
+		w = int(self.pw * self.scale)
+		h = int(self.ph * self.scale)
 		color, depth, id = glGenTextures(3)
 		glBindTexture(GL_TEXTURE_2D, color)
 		glBindTexture(GL_TEXTURE_2D, depth)
@@ -90,6 +137,27 @@ class ChiselView(QtWidgets.QOpenGLWidget):
 
 	def mouseMoveEvent(self, event):
 		super(ChiselView, self).mouseMoveEvent(event)
+		if event.buttons():
+			return
+		self.mouse = event.pos().x(), event.pos().y()
+		self.updateMouse()
+
+	def mousePressEvent(self, event):
+		global sel, part
+		if event.button() != QtCore.Qt.LeftButton:
+			return super(ChiselView, self).mousePressEvent(event)
+		if not sel:
+			return
+		self.mouse = None
+		p, x, y, z, f = sel
+		part.data[z, y, x] = 0
+		part.updateGL()
+		updateViews()
+
+	def mouseReleaseEvent(self, event):
+		global sel
+		if event.button() != QtCore.Qt.LeftButton:
+			return super(ChiselView, self).mouseReleaseEvent(event)
 		self.mouse = event.pos().x(), event.pos().y()
 		self.updateMouse()
 
@@ -108,6 +176,7 @@ class ChiselView(QtWidgets.QOpenGLWidget):
 		self.shader = shaders.compileProgram(vs, gs, fs)
 		self.fbt = []
 		self.fb = glGenFramebuffers(1)
+		part.initGL()
 
 	def resizeGL(self, w, h):
 		self.pw = w
@@ -116,9 +185,14 @@ class ChiselView(QtWidgets.QOpenGLWidget):
 		self.sh = h / min(w, h)
 		self.updateFramebuffer()
 
+	def beforePaint(self):
+		pass
+
 	def paintGL(self):
 		try:
-			w, h = self.pw, self.ph
+			self.beforePaint()
+			w = int(self.pw * self.scale)
+			h = int(self.ph * self.scale)
 			scale = 1.0
 			m = np.diag([-1, 1, 1, 1])
 			v = self.rotate
@@ -126,11 +200,15 @@ class ChiselView(QtWidgets.QOpenGLWidget):
 
 			fb = glGetInteger(GL_DRAW_FRAMEBUFFER_BINDING)
 			glBindFramebuffer(GL_FRAMEBUFFER, self.fb)
+			glViewport(0, 0, w, h)
 
 			glClearColor(0.0, 0.0, 0.0, 0.0)
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 			glEnable(GL_DEPTH_TEST)
 			glEnable(GL_CULL_FACE)
+			glEnable(GL_BLEND)
+			glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
+			glDepthFunc(GL_LESS)
 			glLineWidth(2.0)
 
 			glMatrixMode(GL_PROJECTION)
@@ -151,34 +229,56 @@ class ChiselView(QtWidgets.QOpenGLWidget):
 			#glDrawBuffers(2, [GL_NONE, GL_COLOR_ATTACHMENT1])
 			glDrawBuffers(2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
 			glClearBufferiv(GL_COLOR, 1, (255, 255, 255, 255))
-			glEnableVertexAttribArray(0)
+
 			glUseProgram(self.shader)
+			glBindTextureUnit(0, part.tex)
+
+			glEnableVertexAttribArray(0)
 			glUniformMatrix4fv(0, 1, True, p * v * m)
 			glUniform1i(1, 0)
 			glUniform4f(2, 0, 1, 0, 1)
 			glVertexAttribIPointer(0, 3, GL_INT, 0, Part.indices)
 			glDrawArrays(GL_POINTS, 0, 16*16*16)
 			glDisableVertexAttribArray(0)
+
 			glDrawBuffer(GL_COLOR_ATTACHMENT0)
 			if sel:
 				glDepthFunc(GL_LEQUAL)
-				glUniform4f(2, 1, 0, 0, 1)
-				glVertexAttribI3i(0, *sel[1:])
+				glUniform4f(2, 1, 0, 0, 0.5)
+				glVertexAttribI3i(0, *sel[1:4])
 				glDrawArrays(GL_POINTS, 0, 1)
 			glUseProgram(0)
 
 			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fb)
 			glReadBuffer(GL_COLOR_ATTACHMENT0)
-			glBlitFramebuffer(0, 0, w, h, 0, 0, w, h, GL_COLOR_BUFFER_BIT, GL_NEAREST)
+			glBlitFramebuffer(0, 0, w, h, 0, 0, self.pw, self.ph, GL_COLOR_BUFFER_BIT, GL_LINEAR if self.scale > 1 else GL_NEAREST)
 
-			self.img = np.ndarray((h, w, 4), 'uint8')
-			glGetTextureImage(self.fbt[2], 0, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE, self.img.size, self.img)
+			img = np.ndarray((h, w, 4), 'uint8')
+			glGetTextureImage(self.fbt[2], 0, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE, img.size, img)
+			self.img = img # FIXME scale
 			glInvalidateFramebuffer(GL_READ_FRAMEBUFFER, 2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
 			self.updateMouse()
 
 		except:
 			traceback.print_exc()
 			exit(1)
+
+class WindowLoader(QtUiTools.QUiLoader):
+	def __init__(self, root):
+		super(WindowLoader, self).__init__()
+		self.root = root
+
+	def createWidget(self, className, parent, name):
+		if not parent and self.root:
+			return self.root
+		return super(WindowLoader, self).createWidget(className, parent, name)
+
+class ChiselWindow(QtWidgets.QMainWindow):
+	def __init__(self):
+		super(ChiselWindow, self).__init__()
+		loader = WindowLoader(self)
+		loader.registerCustomWidget(ChiselView)
+		loader.load('mainwindow.ui')
 
 f = QtGui.QSurfaceFormat()
 f.setRenderableType(f.OpenGL)
@@ -189,11 +289,16 @@ f.setOption(f.DebugContext)
 f.setProfile(f.CompatibilityProfile)
 QtGui.QSurfaceFormat.setDefaultFormat(f)
 
+QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
 app = QtWidgets.QApplication(sys.argv)
 
-loader = QtUiTools.QUiLoader()
-loader.registerCustomWidget(ChiselView)
-mainWindow = loader.load('mainwindow.ui')
+mainWindow = ChiselWindow()
+
+def updateViews():
+	mainWindow.viewTop.update()
+	mainWindow.viewRight.update()
+	mainWindow.viewFront.update()
+	mainWindow.viewUser.update()
 
 mainWindow.viewTop.rotate = np.mat([[1,0,0,0], [0,0,1,0], [0,-1,0,0], [0,0,0,1]])
 mainWindow.viewFront.rotate = np.mat([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]])
@@ -202,18 +307,19 @@ mainWindow.viewUser.rotate = np.mat([[1,0,0,0], [0,0.866,0.500,0], [0,-0.500,0.8
 
 angle = 45.0
 start_time = time.time()
-def rotateIt():
+def rotateIt(self: ChiselView):
 	global angle
 	t = time.time() - start_time
-	angle = 45.0 + 60.0 * t
+	#angle = 45.0 + 60.0 * t
 	c = cos(angle * pi / 180.0)
 	s = sin(angle * pi / 180.0)
-	mainWindow.viewUser.rotate = np.mat([[1,0,0,0], [0,0.866,0.500,0], [0,-0.500,0.866,0], [0,0,0,1]]) * np.mat([[c,0,-s,0], [0,1,0,0], [s,0,c,0], [0,0,0,1]])
-	mainWindow.viewUser.update()
+	self.rotate = np.mat([[1,0,0,0], [0,0.866,0.500,0], [0,-0.500,0.866,0], [0,0,0,1]]) * np.mat([[c,0,-s,0], [0,1,0,0], [s,0,c,0], [0,0,0,1]])
 
-timer = QtCore.QTimer(app)
-timer.timeout.connect(rotateIt)
-timer.start()
+#mainWindow.viewUser.beforePaint = types.MethodType(rotateIt, mainWindow.viewUser)
+
+#timer = QtCore.QTimer(app)
+#timer.timeout.connect(mainWindow.viewUser.update)
+#timer.start()
 
 mainWindow.show()
 
--- part.geom.glsl
+++ part.geom.glsl
@@ -5,6 +5,7 @@ layout(triangle_strip, max_vertices = 24) out;
 
 layout(location = 0) uniform mat4 mvp;
 layout(location = 1) uniform int part_id = 0;
+layout(binding = 0) uniform usampler3D part_shape;
 
 in PerVertex
 {
@@ -14,46 +15,51 @@ in PerVertex
 out vec4 color;
 out flat ivec4 id;
 
+struct Face {
+	ivec3 u, v, w;
+	float shade;
+};
+const Face faces[6] = {
+	{ {1, 0, 0}, {0, 1, 0}, {0, 0, 1}, 0.84 },
+	{ {0, 0, 1}, {1, 0, 0}, {0, 1, 0}, 1.00 },
+	{ {0, 1, 0}, {0, 0, 1}, {1, 0, 0}, 0.67 },
+	{ {0, 1, 0}, {1, 0, 0}, {0, 0, -1}, 0.84 },
+	{ {1, 0, 0}, {0, 0, 1}, {0, -1, 0}, 0.45 },
+	{ {0, 0, 1}, {0, 1, 0}, {-1, 0, 0}, 0.67 },
+};
+
 ivec3 pos;
 
-void MakeVertex(ivec3 off, float br) {
-	gl_Position = mvp * vec4((vec3(pos + off - 8) - 0.5) / 16.0, 1.0);
+void MakeVertex(int face_id, ivec3 off, float br) {
+	vec3 vp = vec3(pos + off - 8) * (1.0 / 16.0);
+	gl_Position = mvp * vec4(vp, 1.0);
 	color = vec4(br.xxx, 1.0);
-	id = ivec4(pos, part_id);
+	id = ivec4(pos, part_id | (face_id << 5));
 	EmitVertex();
+}
+
+void MakeFace(int id) {
+	Face face = faces[id];
+	ivec3 w = face.w;
+	ivec3 pos2 = pos + w;
+	if (all(greaterThanEqual(pos2, ivec3(0))) && all(lessThan(pos2, textureSize(part_shape, 0)))) {
+		uint type2 = texelFetch(part_shape, pos + w, 0).x;
+		if (type2 != 0)
+			return;
+	}
+	ivec3 off = clamp(w, 0, 1);
+	MakeVertex(id, off, face.shade);
+	MakeVertex(id, off + face.u, face.shade);
+	MakeVertex(id, off + face.v, face.shade);
+	MakeVertex(id, off + face.u + face.v, face.shade);
+	EndPrimitive();
 }
 
 void main() {
 	pos = vert[0].pos;
-
-	MakeVertex(ivec3(1, 0, 0), 0.67);
-	MakeVertex(ivec3(1, 1, 0), 0.67);
-	MakeVertex(ivec3(1, 0, 1), 0.67);
-	MakeVertex(ivec3(1, 1, 1), 0.67);
-	EndPrimitive();
-	MakeVertex(ivec3(0, 0, 0), 0.67);
-	MakeVertex(ivec3(0, 0, 1), 0.67);
-	MakeVertex(ivec3(0, 1, 0), 0.67);
-	MakeVertex(ivec3(0, 1, 1), 0.67);
-	EndPrimitive();
-	MakeVertex(ivec3(0, 1, 0), 1.00);
-	MakeVertex(ivec3(0, 1, 1), 1.00);
-	MakeVertex(ivec3(1, 1, 0), 1.00);
-	MakeVertex(ivec3(1, 1, 1), 1.00);
-	EndPrimitive();
-	MakeVertex(ivec3(0, 0, 0), 0.45);
-	MakeVertex(ivec3(1, 0, 0), 0.45);
-	MakeVertex(ivec3(0, 0, 1), 0.45);
-	MakeVertex(ivec3(1, 0, 1), 0.45);
-	EndPrimitive();
-	MakeVertex(ivec3(0, 0, 1), 0.84);
-	MakeVertex(ivec3(1, 0, 1), 0.84);
-	MakeVertex(ivec3(0, 1, 1), 0.84);
-	MakeVertex(ivec3(1, 1, 1), 0.84);
-	EndPrimitive();
-	MakeVertex(ivec3(0, 0, 0), 0.84);
-	MakeVertex(ivec3(0, 1, 0), 0.84);
-	MakeVertex(ivec3(1, 0, 0), 0.84);
-	MakeVertex(ivec3(1, 1, 0), 0.84);
-	EndPrimitive();
+	uint type = texelFetch(part_shape, pos, 0).x;
+	if (type == 0)
+		return;
+	for (int k = 0; k < 6; k++)
+		MakeFace(k);
 }
